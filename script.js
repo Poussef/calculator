af344cd9b0dfb307a1cbd30741828688f0069657
Youssef Tawil <yousseftawil321@gmail.com>
2024-02-01 16:56:32 -0800

    added commit function(not working)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let numberOnScreen = 0;
+let numOnScreen = 0;
 
 function add (num1, num2) {
     return num1 + num2;
@@ -8,10 +8,22 @@ function subtract (num1, num2) {
     return num1 - num2;
 };
 
-function multiple (num1, num2) {
+function multiply (num1, num2) {
     return num1 * num2;
 };
 
 function divide (num1, num2) {
     return num1 / num2;
+};
+
+function operate (operator, num1, num2) {
+    if (operator === "+") {
+        add(num1, num2);
+    } else if (operator === "-") {
+        subtract(num1, num2) 
+    } else if (operator === "*") {
+        multiply(num1, num2)
+    } else if (operator === "/") {
+        divide(num1, num2)
+    }
 };
