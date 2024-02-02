91788985cc708e771aa62f2d3c56e308d5fee9fc
Youssef Tawil <yousseftawil321@gmail.com>
2024-02-01 17:03:28 -0800

    operate function working

--- script.js
+++ script.js
@@ -17,13 +17,13 @@ function divide (num1, num2) {
 };
 
 function operate (operator, num1, num2) {
-    if (operator === "+") {
-        add(num1, num2);
-    } else if (operator === "-") {
-        subtract(num1, num2) 
-    } else if (operator === "*") {
-        multiply(num1, num2)
-    } else if (operator === "/") {
-        divide(num1, num2)
+    if (operator == "add") {
+        return add(num1, num2);
+    } else if (operator == "subtract") {
+        return subtract(num1, num2) 
+    } else if (operator == "multiply") {
+        return multiply(num1, num2)
+    } else if (operator == "divide") {
+        return divide(num1, num2)
     }
 };
