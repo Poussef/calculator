335f1198e3d1bee13efaa387ec242a152a7281ce
Youssef Tawil <yousseftawil321@gmail.com>
2024-02-03 18:21:08 -0800

    numbers and operators work and are stored

--- script.js
+++ script.js
@@ -17,20 +17,19 @@ function divide (num1, num2) {
 };
 
 function operate (operator, num1, num2) {
-    if (operator == "add") {
+    if (operator == "+") {
         return add(num1, num2);
-    } else if (operator == "subtract") {
+    } else if (operator == "-") {
         return subtract(num1, num2) 
-    } else if (operator == "multiply") {
+    } else if (operator == "x") {
         return multiply(num1, num2)
-    } else if (operator == "divide") {
+    } else if (operator == "÷") {
         return divide(num1, num2)
     }
 };
 
 const display = document.getElementById('output');
 const numbBtns = document.querySelectorAll(".numbers");
-
 numbBtns.forEach(btn => {
    btn.addEventListener("click", () => {
         if (display.innerHTML[0] === "0") {
@@ -41,6 +40,18 @@ numbBtns.forEach(btn => {
    });
 });
 
+const operatorBtns = document.querySelectorAll('.operators');
+let num1 = 0;
+let num2 = 0;
+let operatorActive = false;
+let operator = "";
+operatorBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        operator = btn.innerHTML;
+        operatorActive = true;
+        num1 = display.innerHTML;
+    });
+});
+
 const clearBtn = document.getElementsByClassName('clear')[0];
 clearBtn.addEventListener("click", () => display.innerHTML = "0");
-
