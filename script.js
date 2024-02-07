4e8f46601dbbbcd1b4cb3bc29db3f3682bf1416a
Youssef Tawil <yousseftawil321@gmail.com>
2024-02-06 16:38:11 -0800

    added a line of nothing for nvim test

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
 let numOnScreen = 0;
 
-function add (num1, num2) {
-    return num1 + num2;
+function add(num1, num2) {
+  return num1 + num2;
 };
 
-function subtract (num1, num2) {
-    return num1 - num2;
+
+function subtract(num1, num2) {
+  return num1 - num2;
 };
 
-function multiply (num1, num2) {
-    return num1 * num2;
+function multiply(num1, num2) {
+  return num1 * num2;
 };
 
-function divide (num1, num2) {
-        return num1 / num2;
+function divide(num1, num2) {
+  return num1 / num2;
 };
 
-function operate (operator, num1, num2) {
-    if (operator == "+") {
-        return add(num1, num2);
-    } else if (operator == "-") {
-        return subtract(num1, num2) 
-    } else if (operator == "x") {
-        return multiply(num1, num2)
-    } else if (operator == "÷") {
-        return divide(num1, num2)
-    }
+function operate(operator, num1, num2) {
+  if (operator == "+") {
+    return add(num1, num2);
+  } else if (operator == "-") {
+    return subtract(num1, num2)
+  } else if (operator == "x") {
+    return multiply(num1, num2)
+  } else if (operator == "÷") {
+    return divide(num1, num2)
+  }
 };
 
 const display = document.getElementById('output');
 const numbBtns = document.querySelectorAll(".numbers");
 numbBtns.forEach(btn => {
-   btn.addEventListener("click", () => {
-        if (display.innerHTML[0] === "0") {
-            display.innerHTML = btn.innerHTML;
-        } else if (operatorActive === true) {
-            display.innerHTML = btn.innerHTML;
-            operatorActive = false;
-        } else {
-            display.innerHTML += btn.innerHTML; 
-        };
-   });
+  btn.addEventListener("click", () => {
+    if (display.innerHTML[0] === "0") {
+      display.innerHTML = btn.innerHTML;
+    } else if (operatorActive === true) {
+      display.innerHTML = btn.innerHTML;
+      operatorActive = false;
+    } else {
+      display.innerHTML += btn.innerHTML;
+    };
+  });
 });
 
 const operatorBtns = document.querySelectorAll('.operators');
@@ -49,24 +50,24 @@ let num2 = 0;
 let operatorActive = false;
 let operator = "";
 operatorBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        operator = btn.innerHTML;
-        operatorActive = true;
-        num1 = display.innerHTML;
-    });
+  btn.addEventListener("click", () => {
+    operator = btn.innerHTML;
+    operatorActive = true;
+    num1 = display.innerHTML;
+  });
 });
 
 const equalsBtn = document.getElementById('equals');
 equalsBtn.addEventListener("click", () => {
-    if (!operator) {
-        return
-    } else {
+  if (!operator) {
+    return
+  } else {
     num2 = display.innerHTML;
     display.innerHTML = operate(operator, parseInt(num1), parseInt(num2));
     num1 = 0;
     num2 = 0;
     operator = "";
-    }
+  }
 })
 
 const clearBtn = document.getElementsByClassName('clear')[0];
